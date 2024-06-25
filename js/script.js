36f1c960588aae768551a06829d7483cd1fa2416
tycoonkabuzi <103669803+tycoonkabuzi@users.noreply.github.com>
2024-06-25 16:09:18 +0200

    Created a possibility, when we click on more, being able to retrieve the data from the textContent but also excluding the button more

--- js/script.js
+++ js/script.js
@@ -1,10 +1,24 @@
 const howLong = document.querySelector(
   ".main__content__inputs__howLong__container"
 );
-const addtime = document.getElementById("add");
+const addTime = document.getElementById("add");
+
+// create a possibility to click on a time and have it selected in a different color
 howLong.addEventListener("click", (e) => {
   const button = howLong.querySelectorAll("li");
   button.forEach((button) => button.classList.remove("clicked"));
   const target = e.target;
   target.classList.add("clicked");
+});
+
+// when clicking on more, being able to retrieve the data from the list and multiply it or change it.
+addTime.addEventListener("click", () => {
+  const button = howLong.querySelectorAll("li");
+  button.forEach((button) => {
+    // remove more from the list of numbers to be multiplied.
+    if (button.textContent != "More") {
+      const theTime = button.textContent.replace(/min|h/g, "");
+      console.log(theTime);
+    }
+  });
 });
